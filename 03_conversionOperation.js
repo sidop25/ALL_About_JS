6091559671332b5fd053c64dbe7bbc692e4d4128
sidop25 <113361370+sidop25@users.noreply.github.com>
2023-12-02 16:28:21 +0530

    Add files via upload

---
+++ 03_conversionOperation.js
@@ -0,0 +1,43 @@
+// let score = 33
+// console.log(typeof score);  // o/p- number
+
+// let score2 = "32"
+// console.log(typeof score2);  // o/p - string
+
+// //conversion of datatype - important for backend stuff
+// let valueInNumber = Number(score2) // imp - original the datatype of score2 is string but if we need number for some purpose
+// // we can convert the datatype of variable 
+// console.log( typeof valueInNumber);  // o/p number original the value stored in the variable is string 
+
+// // suppose in a variable string value 123ab is stored and we have to convert it to number
+// let score_3 = "123ab"
+// // console.log(typeof score_3);  // o/p tyoeof - string now converting
+// let valueScore_3 = Number(score_3)
+// console.log(typeof valueScore_3) // o/p Number 
+// console.log(valueScore_3); // o/p - NaN // NaN is converted into Number datatype so check always
+
+// let booleanNum =  2 // "sid" //  0 //1
+// let boolena_num = Boolean(booleanNum)
+// console.log(boolena_num); 
+
+/*
+1 => true
+2 => true 
+0 => false
+string ("sid") => true
+*/
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber) // o/p 33 in the form of string ("33")
+// console.log(typeof stringNumber);
+
+// it goes L-R 
+console.log("1"+2); // string + int = 12
+console.log(1+"2");  // 12
+console.log("1"+2+2); //122 string+int+int "1"+2+2 = 122
+console.log(1+2+"2"); // int+int = int + string 1+2 =3 + "2 = 32"
+
+// learn about the prefix and postfix precendece in javascript src - mdn or w3
+
+
